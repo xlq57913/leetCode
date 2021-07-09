305b620868325671e38a02fee2d222098e1cd80a
xlq57913 <xlq57913@126.com>
2021-07-09 14:08:34 +0800

    solve p27

---
+++ 27.移除元素.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=cpp
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int length = nums.size(), fast = 0, slow = 0;
+        while(fast < length) {
+            if(nums[fast]!=val){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+};
+// @lc code=end
+
